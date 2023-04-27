c331effc8d8a22d9136c3897c8db74348ad249f3
caesarals <slac_@hotmail.com>
2023-04-26 22:09:46 -0400

    incremento salarial terminado

---
+++ incremento.rb
@@ -0,0 +1,21 @@
+# 1. Requerimiento 1, se solicita un programa que mediante el uso de métodos permita
+# calcular el nuevo salario de un colaborador/a en función de un porcentaje mínimo de
+# aumento establecido.
+
+puts
+puts "Por favor ingrese su salario"
+salary = gets.chomp.to_f
+puts
+puts "Por favor ingrese el porcentaje a incrementar"
+salary_increase_percentage = gets.chomp.to_f 
+puts
+
+def get_new_salary(salary, increase)
+    actual_percentage = increase/100
+    puts actual_percentage
+    increase_salary = salary * actual_percentage
+    new_salary = salary + increase_salary 
+    return new_salary
+end
+
+puts "El nuevo salario es #{get_new_salary(salary, salary_increase_percentage)}"
